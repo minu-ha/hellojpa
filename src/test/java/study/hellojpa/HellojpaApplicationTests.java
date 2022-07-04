d4feced295b8161f254659a48ef537f05fdb591e
wumeenna <minu0604@naver.com>
2022-07-04 16:31:13 +0900

    Initial commit

---
+++ src/test/java/study/hellojpa/HellojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojpaApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
